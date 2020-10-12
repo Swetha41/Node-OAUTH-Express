09e5d7728e21ea3e566d0343fc8ba7d99529ff71
unknown <swethagottimukkala@gmail.com>
2020-10-12 22:11:31 +0530

    route added

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const app = express();
 //set up view engine
 app.set('view engine', 'ejs');
 
+//routes
+app.get("/", (req, res) => {
+    res.send('Hello World');
+})
+
 app.listen(3000, () => {
     console.log(`server started at port 3000`)
 })
