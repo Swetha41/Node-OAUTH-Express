bafba7cfa2e98d30e0db2acc6b449645a23fa8b5
unknown <swethagottimukkala@gmail.com>
2020-10-13 12:48:43 +0530

    login route added

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const ejs = require('ejs');
+const authRoutes = require('./routes/auth-routes');
 
 const app = express();
 
 //set up view engine
 app.set('view engine', 'ejs');
 
-//routes
+//set up routes
+app.use('/auth', authRoutes);
+
+//home route
 app.get("/", (req, res) => {
     res.render('home');
 })
